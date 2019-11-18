9b6948047dea7bccf3c4ce40528dd6738eb930af
dragon <art_c@bk.ru>
2019-11-18 17:30:04 +0600

    Сatalog restructuring

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const app = express()
+const cookieParser = require('cookie-parser')
 const connectDB = require('./config/db')
 
+const User = require('./models/user')
 
 connectDB()
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cookieParser())
+
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, userData) => {
+        if (err)
+            return res.json({ success: false, err })
+    })
+
+    return res.status(200).json({ success: true })
 })
 
 app.listen(5000, () => {
